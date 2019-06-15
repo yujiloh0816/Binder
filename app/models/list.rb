23012be52d1826eb1b1a69eb819d70baad4581d8
yujiro fujiwara <imp.yfujiwara@gmail.com>
2019-06-15 13:37:29 +0900

    アソシエーションを追加

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ApplicationRecord
+  has_many :inspections
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_many :lists
+  belongs_to :company
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
