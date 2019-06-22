048bcbd5d00877148e5ebc8c7f37881491093f9a
yujiro fujiwara <imp.yfujiwara@gmail.com>
2019-06-22 21:49:02 +0900

    seedsデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Company.create!(
+  name: "ディップ株式会社",
+  domain: "https://www.dip-net.co.jp/",
+  img_name: open("#{Rails.root}/app/assets/images/comapnies/dip.jpg")
+)
+Company.create!(
+  name: "パーソルキャリア株式会社",
+  domain: "https://www.persol-career.co.jp/",
+  img_name: open("#{Rails.root}/app/assets/images/comapnies/persol.jpg")
+)
+Company.create!(
+  name: "マイナビ株式会社",
+  domain: "https://job.mynavi.jp/",
+  img_name: open("#{Rails.root}/app/assets/images/comapnies/mynavi.jpg")
+)
+Company.create!(
+  name: "リクルート株式会社",
+  domain: "https://www.recruit.co.jp/",
+  img_name: open("#{Rails.root}/app/assets/images/comapnies/recruit.jpg")
+)
+
+User.create!(
+  name: "Bob",
+  email: "user1@example.com",
+  password: "hogehoge"
+)
