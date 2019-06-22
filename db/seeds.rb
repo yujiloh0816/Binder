b71a7913e8b0a738bd8545c43fe56aea5fd95dc3
yujiro fujiwara <imp.yfujiwara@gmail.com>
2019-06-22 21:55:02 +0900

    seedsデータ微修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
 Company.create!(
   name: "ディップ株式会社",
   domain: "https://www.dip-net.co.jp/",
-  img_name: open("#{Rails.root}/app/assets/images/comapnies/dip.jpg")
+  img_name: open("#{Rails.root}/app/assets/images/companies/dip.jpg")
 )
 Company.create!(
   name: "パーソルキャリア株式会社",
   domain: "https://www.persol-career.co.jp/",
-  img_name: open("#{Rails.root}/app/assets/images/comapnies/persol.jpg")
+  img_name: open("#{Rails.root}/app/assets/images/companies/persol.jpg")
 )
 Company.create!(
   name: "マイナビ株式会社",
   domain: "https://job.mynavi.jp/",
-  img_name: open("#{Rails.root}/app/assets/images/comapnies/mynavi.jpg")
+  img_name: open("#{Rails.root}/app/assets/images/companies/mynavi.jpg")
 )
 Company.create!(
   name: "リクルート株式会社",
   domain: "https://www.recruit.co.jp/",
-  img_name: open("#{Rails.root}/app/assets/images/comapnies/recruit.jpg")
+  img_name: open("#{Rails.root}/app/assets/images/companies/recruit.jpg")
 )
 
 User.create!(
