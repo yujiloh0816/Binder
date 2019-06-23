a959ce0737ddde49503624c495b07eb2dc4e0cae
yujiro fujiwara <imp.yfujiwara@gmail.com>
2019-06-23 14:00:21 +0900

    cron設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,11 @@
-# Use this file to easily define all of your cron jobs.
+# cronにデータを反映
+# bundle exec whenever --update-crontab
 
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-
-# set :output, "/path/to/my/cron_log.log"
-
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
-every 1.minute do
-  command "echo 'mossmossmossmossmossmoss'"
+every 1.days do
+  command "curl -L http://localhost:3000/api/v1/companies"
 end
+
+# 確認用
+# every 1.minutes do
+#   command "curl -L http://localhost:3000/api/v1/companies"
+# end
