d608250e33c9e932984ef87d525586ef10c3bf3b
yujiro fujiwara <imp.yfujiwara@gmail.com>
2019-06-29 01:56:34 +0900

    コメント追加

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CompaniesController < ApplicationController
 
 # ToDo apiとしてバッチ処理をすべきか検討
+# callbackでcheck_http_statusを実行
   def regularly_check_companies
     companies = Company.all
     companies.each do |company|
