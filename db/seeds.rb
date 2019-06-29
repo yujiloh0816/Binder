6f85faed96e39bd0ca8f86bef3a762aebe28f1f0
yujiro fujiwara <imp.yfujiwara@gmail.com>
2019-06-29 16:13:56 +0900

    seedsファイル変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,65 +1,67 @@
-test_user = User.create!(
-  name: "taro",
-  email: "taro@example.com",
-  password: "hogehoge"
-)
+ActiveRecord::Base.transaction do
+  test_user = User.create!(
+    name: "taro",
+    email: "taro@example.com",
+    password: "hogehoge"
+  )
 
-company1 = Company.create!(
-  name: "ディップ株式会社",
-  domain: "https://www.dip-net.co.jp/",
-  img_name: open("#{Rails.root}/db/dummy_img/dip.jpg")
-)
+  company1 = Company.create!(
+    name: "ディップ株式会社",
+    domain: "https://www.dip-net.co.jp/",
+    img_name: open("#{Rails.root}/db/dummy_img/dip.jpg")
+  )
 
-user1 = company1.users.create!(
-  name: "Bob",
-  email: "bob@example.com",
-  password: "hogehoge"
-)
+  user1 = company1.users.create!(
+    name: "Bob",
+    email: "bob@example.com",
+    password: "hogehoge"
+  )
 
-Reaction.create!(
-  to_user_id: test_user.id,
-  from_user_id: user1.id,
-  status: "good"
-)
+  Reaction.create!(
+    to_user_id: test_user.id,
+    from_user_id: user1.id,
+    status: "good"
+  )
 
-company2 = Company.create!(
-  name: "パーソルキャリア株式会社",
-  domain: "https://www.persol-career.co.jp/",
-  img_name: open("#{Rails.root}/db/dummy_img/persol.jpg")
-)
+  company2 = Company.create!(
+    name: "パーソルキャリア株式会社",
+    domain: "https://www.persol-career.co.jp/",
+    img_name: open("#{Rails.root}/db/dummy_img/persol.jpg")
+  )
 
-user2 = company2.users.create!(
-  name: "Tom",
-  email: "tom@example.com",
-  password: "hogehoge"
-)
+  user2 = company2.users.create!(
+    name: "Tom",
+    email: "tom@example.com",
+    password: "hogehoge"
+  )
 
-Reaction.create!(
-  to_user_id: test_user.id,
-  from_user_id: user2.id,
-  status: "good"
-)
+  Reaction.create!(
+    to_user_id: test_user.id,
+    from_user_id: user2.id,
+    status: "good"
+  )
 
-company3 = Company.create!(
-  name: "マイナビ株式会社",
-  domain: "https://job.mynavi.jp/",
-  img_name: open("#{Rails.root}/db/dummy_img/mynavi.jpg")
-)
+  company3 = Company.create!(
+    name: "マイナビ株式会社",
+    domain: "https://job.mynavi.jp/",
+    img_name: open("#{Rails.root}/db/dummy_img/mynavi.jpg")
+  )
 
-company3.users.create!(
-  name: "Alex",
-  email: "alex@example.com",
-  password: "hogehoge"
-)
+  company3.users.create!(
+    name: "Alex",
+    email: "alex@example.com",
+    password: "hogehoge"
+  )
 
-company4 = Company.create!(
-  name: "リクルート株式会社",
-  domain: "https://www.recruit.co.jp/",
-  img_name: open("#{Rails.root}/db/dummy_img/recruit.jpg")
-)
+  company4 = Company.create!(
+    name: "リクルート株式会社",
+    domain: "https://www.recruit.co.jp/",
+    img_name: open("#{Rails.root}/db/dummy_img/recruit.jpg")
+  )
 
-company4.users.create!(
-  name: "Jane",
-  email: "jane@example.com",
-  password: "hogehoge"
-)
+  company4.users.create!(
+    name: "Jane",
+    email: "jane@example.com",
+    password: "hogehoge"
+  )
+end
